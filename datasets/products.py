393d5efc9eed4634618aeaf2101d65ce0c56a2eb
Jhean Daza <60199238+seandaza@users.noreply.github.com>
2021-09-30 12:22:03 -0500

    Add files via upload

---
+++ datasets/products.py
@@ -0,0 +1,7 @@
+products = [
+    { "name": "laptop", "price": 800, "quantity": 20 },
+    { "name": "mouse", "price": 12, "quantity": 120 },
+    { "name": "monitor", "price": 1800, "quantity": 34 }
+    
+
+]
