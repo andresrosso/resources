b3887e3e9e2cdaa5aa1c559ca88b22f10fee9939
Jhean Daza <60199238+seandaza@users.noreply.github.com>
2021-09-30 12:22:26 -0500

    Delete products.py

--- products.py
+++
@@ -1,7 +0,0 @@
-products = [
-    { "name": "laptop", "price": 800, "quantity": 20 },
-    { "name": "mouse", "price": 12, "quantity": 120 },
-    { "name": "monitor", "price": 1800, "quantity": 34 }
-    
-
-]
